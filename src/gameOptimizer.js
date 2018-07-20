3c9c6ed230ae9ea16ed999d9b411d383e50e8ff9
Sergey Kostikov <sergey.diod@gmail.com>
2018-07-20 17:32:21 +0300

    gameOptimizer.js

--- src/gameOptimizer.js
+++ src/gameOptimizer.js
@@ -3,8 +3,74 @@ class Army{
         this.name = options.name;
         this.titanPower = options.titanPower;
         this.heroPower = options.heroesPower;
-        this.titanCounter = option.titanCounter;
-        this.heroCounter = option.heroCounter;
+        this.titanCounter = options.titanCounter;
+        this.heroCounter = options.heroCounter;
     }
 }
 
+let army = []
+let enemy = []
+
+count = 0;
+
+while(count < 5){
+    let armyObj = new Army({name:`army_${count+1}`,titanPower:Math.floor(Math.random()*1000),heroesPower:Math.floor(Math.random()*1000),titanCounter:1,heroCounter:1})
+    army.push(armyObj)
+
+    let enemyObj = new Army({name:`enemy_${count+1}`,titanPower:Math.floor(Math.random()*1000),heroesPower:Math.floor(Math.random()*1000),titanCounter:1,heroCounter:1})
+    enemy.push(enemyObj)
+    count ++;
+}
+
+function compare(army,enemy){
+    let resulArr = []
+    let strikeArmies = [];
+    
+    army.map(armyElem => {
+        let enemyArmyName = '';
+        let enemyArmyHeroPower = 0;
+        let enemyArmyTitanPower = 0;
+        let resultObj = {}
+        let tmpArr = []
+
+
+
+        enemy.filter(elem => {
+            if(!strikeArmies.includes(elem.name)){
+                tmpArr.push(elem)
+            }
+        })
+        console.log('tmpArr',tmpArr)
+        tmpArr.map(enemyElem => {
+            if(enemyArmyName == '' && enemyArmyHeroPower == 0){
+                enemyArmyName = enemyElem.name;
+                enemyArmyHeroPower = enemy.heroPower;
+            }
+            if(armyElem.heroPower > enemyElem.heroPower){
+                if(enemyArmyHeroPower > enemyElem.heroPower){
+                    enemyArmyName = enemyElem.name;
+                    enemyArmyHeroPower = enemy.heroPower;
+                }
+            }
+            
+        })
+        let whomStrike =''
+        if(armyElem.heroPower > enemyArmyHeroPower){
+            whomStrike = enemyArmyName
+            strikeArmies.push(enemyArmyName) 
+        }else{
+            whomStrike = 'Nobody'
+        }
+        resultObj.whoStrikeHeroes = armyElem.name
+        resultObj.whomStrikeHeroes = whomStrike
+        resulArr.push(resultObj)
+              
+    })
+
+    return resulArr
+}
+
+
+console.log(army)
+console.log(enemy)
+console.log(compare(army,enemy))
